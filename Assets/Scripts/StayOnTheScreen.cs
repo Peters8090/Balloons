72bd42ba54342fadecaa2c3f865b1f1b82b84479
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-02-12 15:43:08 +0100

    Torus movement. Physics and boundaries setup

---
+++ Assets/Scripts/StayOnTheScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayOnTheScreen : MonoBehaviour
+{
+    [SerializeField]
+    Renderer renderer;
+
+    float smoothFactor = 0.5f;
+
+    float topBorder = 0.8f;
+
+    void Update()
+    {
+        //https://answers.unity.com/questions/509283/limit-a-sprite-to-not-go-off-screen.html
+        float distance = (transform.position - Camera.main.transform.position).z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + (renderer.bounds.size.x / 2);
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - (renderer.bounds.size.x / 2);
+
+        float bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).y + (renderer.bounds.size.y / 2);
+        //var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance)).y - (renderer.bounds.size.y / 2);
+
+
+        Vector3 targetPos = new Vector3(
+            Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+            Mathf.Clamp(transform.position.y, bottomBorder, topBorder),
+            transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothFactor);
+    }
+}
---
+++ Assets/Scripts/Torus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torus : MonoBehaviour
+{
+    [SerializeField]
+    float movingSensivity = 0.05f;
+    [SerializeField]
+    float rotatingSensitvity = 30f;
+
+    void Update()
+    {
+        #region Moving
+        foreach (var touch in Input.touches)
+        {
+            Vector3 delta = new Vector3(touch.deltaPosition.x, touch.deltaPosition.y, 0);
+            transform.position = Vector3.Lerp(transform.position, transform.position + delta, movingSensivity * Time.deltaTime);
+        } 
+        #endregion
+
+        #region Rotating
+        Quaternion desiredRotation = transform.rotation;
+
+        DetectTouchMovement.Calculate();
+        if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0)
+        {
+            Vector3 rotationDeg = Vector3.zero;
+            rotationDeg.z = DetectTouchMovement.turnAngleDelta;
+            desiredRotation *= Quaternion.Euler(rotationDeg);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotatingSensitvity);
+        #endregion
+    }
+}
