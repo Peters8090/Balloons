b4069669648df2196509dafbb2f8027d47ea24f0
Peters8090 <52279875+Peters8090@users.noreply.github.com>
2020-02-12 12:09:55 +0100

    Cleanup, place and setup objects on the scene

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    Rigidbody rb;
+    float floatingForce = 0.5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rb.AddForce(Vector3.up * floatingForce);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Trap")
+        {
+            rb.AddExplosionForce(1f, transform.position, 1f);
+        }
+    }
+}
